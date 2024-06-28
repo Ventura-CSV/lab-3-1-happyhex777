c643d5b6840e423ecf160b890c7e1faf7f533d63
Hector Diaz <hhectordiazz@icloud.com>
2024-06-28 13:40:09 -0700

    Complete lab3-1

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    number = int(input('Enter your input: '))
-
+    number= int(input('Enter your input: '))
+    if number < 50:
+        range=1
+    elif 50 <= number < 100:
+        range=2
+    else:
+        range=3
     """
+    
     Make your code here
     """
 
